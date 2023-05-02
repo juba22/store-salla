8721c7d10f0d129ffe0c2f521289294844075b4c
juba22 <mjopa84@gmail.com>
2023-05-03 00:23:31 +0300

    upload edite10

--- js/custom.js
+++ js/custom.js
@@ -161,10 +161,9 @@ var shoppingCart = (function() {
     for(var i in cartArray) {
       output += "<tr>"
         + "<td>" + cartArray[i].name + "</td>" 
-        + "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-custom' data-name=" + cartArray[i].name + "><span>-</span></button>"
+        + "<td class='calc-price'><div class='input-group'><button class='minus-item input-group-addon btn btn-custom' data-name=" + cartArray[i].name + "><span>-</span></button>"
         + "<input type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"
-        + "<button class='plus-item btn btn-custom input-group-addon' data-name=" + cartArray[i].name + "><span>+</span></button></div></td>"
-        + "<td><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>"
+        + "<button class='plus-item btn btn-custom input-group-addon' data-name=" + cartArray[i].name + "><span>+</span></button></div><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>"
         + " = " 
         + "<td>" + cartArray[i].total + "</td>" 
         +  "</tr>";
